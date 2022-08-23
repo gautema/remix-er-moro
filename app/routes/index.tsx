e68789a6b69e6bbca64618b7456f5ac36ab421d9
Gaute Magnussen <gaute.magnussen@gmail.com>
2022-08-23 13:30:17 +0200

    Added Link

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,12 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
     <div className="flex flex-col max-w-3xl content-center mx-auto">
       <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <Link to="/place/oslo">
+        <a>Oslo</a>
+      </Link>
     </div>
   );
 }
