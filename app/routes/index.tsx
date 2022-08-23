d77ad10f67008ea537256c3550b318a7c10ac703
Gaute Magnussen <gaute.magnussen@gmail.com>
2022-08-23 15:45:45 +0200

    Prefetch

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <div className="flex flex-col max-w-3xl content-center mx-auto">
       <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
-      <Link to="/place/oslo">
+      <Link to="/place/oslo" prefetch={"intent"}>
         <a>Oslo</a>
       </Link>
     </div>
