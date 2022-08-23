34bb80f99a87e88cf38e38ef7ad5f1dc9a4650e9
Gaute Magnussen <gaute.magnussen@gmail.com>
2022-08-23 13:56:25 +0200

    Added form

--- app/routes/place/$place.tsx
+++ app/routes/place/$place.tsx
@@ -1,14 +1,53 @@
-import { json, LoaderFunction } from "@remix-run/cloudflare";
-import { useLoaderData, useParams } from "@remix-run/react";
+import { ActionFunction, json, LoaderFunction } from "@remix-run/cloudflare";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useTransition,
+} from "@remix-run/react";
 type Data = {
   name: String;
+  favorite: boolean;
 };
 
 export const loader: LoaderFunction = async ({ params }) => {
-  return json<Data>({ name: params.place?.toLocaleUpperCase() || "" });
+  return json<Data>({
+    name: params.place?.toLocaleUpperCase() || "",
+    favorite: false,
+  });
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+  return json<Data>({
+    name: params.place?.toLocaleUpperCase() || "",
+    favorite: true,
+  });
 };
 
 export default function Place() {
   const data = useLoaderData<Data>();
-  return <div>{data.name}</div>;
+  const actionData = useActionData<Data>();
+  const transition = useTransition();
+
+  return (
+    <div>
+      {data.name}
+      {actionData && actionData.favorite && <>Fav</>}
+      {!actionData && data.favorite && <>Fav</>}
+      {actionData && !actionData.favorite && <>Not fav</>}
+      {!actionData && !data.favorite && <>Not fav</>}
+      {transition.state === "idle" && (
+        <Form method="post">
+          <button
+            type="submit"
+            className="border-2 rounded-md border-green-600"
+          >
+            Set favorite
+          </button>
+        </Form>
+      )}
+      {transition.state === "loading" && <>Loading</>}
+      {transition.state === "submitting" && <>Submitting</>}
+    </div>
+  );
 }
